f18410a4885b2e6fefaf0396531652c72600d230
tmgast <tmgast@gmail.com>
2021-10-31 17:42:49 +0900

    functional build with random int

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,25 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
     counter: 0
   },
   mutations: {
-    increaseCounter(state){
-      state.counter++
+    increaseCounter(state, randomNum){
+      console.log(randomNum);
+      state.counter+=randomNum
     },
     decreaseCounter(state){
-      state.counter--
+      state.counter--;
     }
   },
   actions: {
-    increaseCounter(){
+    increaseCounter({commit}){
       console.log("increase counter");
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+        commit('increaseCounter', response.data)
+      })
     }
   },
   getter:{
