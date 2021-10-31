f0b19bbf45c4fb48f406bf10c4158ac2ae25f81f
tmgast <tmgast@gmail.com>
2021-11-01 06:28:10 +0900

    added simple todo list on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,56 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <form @submit.prevent="addNewTodo">
+    <label>New Todo</label>
+    <input type="text" v-model="newTodo" name="newtodo">
+    <button>Add New Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id" class="todo">
+      <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">{{todo.content}}</h3>
+    </li>
+  </ul>
 </template>
+
+<script>
+import {ref} from 'vue';
+
+export default {
+  setup(){
+    const newTodo = ref('');
+    const todos = ref([]);
+
+    function addNewTodo(){
+      console.log(newTodo.value);
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = '';
+    }
+
+    function toggleDone(todo){
+      todo.done = !todo.done;
+    }
+
+    return {
+      toggleDone,
+      todos,
+      newTodo,
+      addNewTodo,
+    }
+  }
+}
+</script>
+
+<style>
+.todo {
+  cursor: pointer;
+}
+
+.done {
+  text-decoration: line-through;
+}
+</style>
